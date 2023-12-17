6dd32ab2e6773e9fba44dec6f89464bb554a032b
YuHaiYang <yu_haiyang@foxmail.com>
2023-12-17 20:19:50 +0800

    feat(project): 添加核心包spring自动装载配置

---
+++ shimmer-core/src/main/java/io/github/shimmer/core/ShimmerCoreConfig.java
@@ -0,0 +1,11 @@
+package io.github.shimmer.core;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "io.github.shimmer.core")
+public class ShimmerCoreConfig {
+
+}
