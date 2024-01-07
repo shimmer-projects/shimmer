ab4de6b1f2bb49625c6a3d07946fac5eb9e99128
YuHaiYang <yu_haiyang@foxmail.com>
2024-01-07 11:44:15 +0800

    feat(utils): 通用常量类

---
+++ shimmer-utils/src/main/java/io/github/shimmer/utils/Constant.java
@@ -0,0 +1,30 @@
+package io.github.shimmer.utils;
+
+/**
+ * <p>
+ * 通用的常量定义
+ * </p>
+ * Created on 2023-12-16 22:15
+ *
+ * @author yu_haiyang
+ */
+public final class Constant {
+
+    public static final Integer NEGATIVE_ONE = -1;
+    public static final Integer ZERO = 0;
+    public static final Integer ONE = 1;
+    public static final Integer TWO = 2;
+    public static final Integer THREE = 3;
+    public static final Integer FOUR = 4;
+    public static final Integer FIVE = 5;
+    public static final Integer SIX = 6;
+    public static final Integer SEVEN = 7;
+    public static final Integer EIGHT = 8;
+    public static final Integer NINE = 9;
+    public static final Integer TEN = 10;
+    public static final String EMPTY_STR = "";
+    public static final String POINT_STR = ".";
+    public static final String SLASH_STR = "/";
+
+
+}
