39c282fabc5db5cdb3d25b3dc3711f2b6d57bfa1
YuHaiYang <yu_haiyang@foxmail.com>
2024-01-13 17:56:28 +0800

    feat(runner): 主启动工程配置打包方式

--- shimmer-runner/src/main/java/io/github/shimmer/runner/controller/IndexController.java
+++ shimmer-runner/src/main/java/io/github/shimmer/runner/controller/IndexController.java
@@ -1,17 +1,22 @@
 package io.github.shimmer.runner.controller;
 
 
-import org.springframework.stereotype.Controller;
+import io.github.shimmer.core.debounce.LimitAccessException;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
 
-
     @GetMapping("hello")
-    @ResponseBody
-    public String hello(String name) {
+    public String hello(@RequestParam String name) {
         return "Hello " + name;
     }
+
+    @GetMapping("void")
+    public void str() {
+        throw new LimitAccessException("轻点，太快了！");
+    }
+
 }
